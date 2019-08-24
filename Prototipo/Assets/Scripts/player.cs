833ffec97f6bfa87b59e11b0ec978be95c69455d
mbpmm <marianomcr92@gmail.com>
2019-08-24 18:26:21 -0300

    Agregado tilemap

--- Prototipo/Assets/Scripts/player.cs
+++ Prototipo/Assets/Scripts/player.cs
@@ -12,20 +12,17 @@ public class player : MonoBehaviour
     public bool exitOnce;
     public Animator animator;
     public int direction;
+    public bool onTheGround;
+    public bool doubleJumpAllowed;
 
     private float powerUpDuration;
     private bool powerUp;
     private Rigidbody2D playerRB;
-    private float limMax, limXMin, limYMin;
-    private float nextFire;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        limMax = 0.96f;
-        limXMin = 0.04f;
-        limYMin = 0.07f;
         exitOnce = true;
     }
 
@@ -38,32 +35,51 @@ public class player : MonoBehaviour
 
         playerRB.velocity = movement;
 
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && playerRB.velocity.y==0)
+        //if (CrossPlatformInputManager.GetButtonDown("Jump") )
+        //{
+        //    playerRB.AddForce(Vector2.up * jumpForce);
+        //}
+
+        //if (horizontal < 0)
+        //{
+        //    direction = 1;
+        //    animator.SetInteger("Direction", direction);
+        //    animator.SetBool("IsMoving", true);
+        //}
+        //if (horizontal > 0)
+        //{
+        //    direction = 2;
+        //    animator.SetInteger("Direction", direction);
+        //    animator.SetBool("IsMoving", true);
+        //}
+        
+        //if (horizontal == 0 && vertical == 0)
+        //{
+        //    direction = 0;
+        //    animator.SetInteger("Direction", direction);
+        //    animator.SetBool("IsMoving", false);
+        //}
+
+        if (playerRB.velocity.y == 0)
+            onTheGround = true;
+        else
+            onTheGround = false;
+
+        if (onTheGround)
+            doubleJumpAllowed = true;
+
+        if (onTheGround && CrossPlatformInputManager.GetButtonDown("Jump"))
         {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 0f);
             playerRB.AddForce(Vector2.up * jumpForce);
         }
-
-        if (horizontal < 0)
+        else if (doubleJumpAllowed && CrossPlatformInputManager.GetButtonDown("Jump"))
         {
-            direction = 1;
-            animator.SetInteger("Direction", direction);
-            animator.SetBool("IsMoving", true);
-        }
-        if (horizontal > 0)
-        {
-            direction = 2;
-            animator.SetInteger("Direction", direction);
-            animator.SetBool("IsMoving", true);
-        }
-        
-        if (horizontal == 0 && vertical == 0)
-        {
-            direction = 0;
-            animator.SetInteger("Direction", direction);
-            animator.SetBool("IsMoving", false);
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 0f);
+            playerRB.AddForce(Vector2.up * jumpForce);
+            doubleJumpAllowed = false;
         }
 
-        
     }
 
     
