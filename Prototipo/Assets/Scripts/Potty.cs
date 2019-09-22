d2148e2fab0e5247d163947906b1833bfc106516
mbpmm <marianomcr92@gmail.com>
2019-09-21 21:44:49 -0300

    Agregado enemigo Potty, bajado intensidad de niebla

---
+++ Prototipo/Assets/Scripts/Potty.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potty : MonoBehaviour
+{
+    public GameObject bulletEmitter;
+    public GameObject bullet;
+    public float fireRate;
+    private float nextShoot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextShoot)
+        {
+            nextShoot = Time.time + fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bulletAux;
+        bulletAux = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation);
+    }
+}
