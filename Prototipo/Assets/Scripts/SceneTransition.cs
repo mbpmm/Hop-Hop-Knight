c2540cfec5d69739398fe166b327346ce2c6b41e
mbpmm <marianomcr92@gmail.com>
2019-10-09 11:49:19 -0300

    Agregado escena de intro

---
+++ Prototipo/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+        //if (levelToLoad==1)
+        //{
+        //    animator.SetTrigger("FadeOut");
+        //}
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("IsClicked");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
