7429a6bb0ec7f29818455b8aa40bf35eccb21204
mbpmm <marianomcr92@gmail.com>
2019-10-15 14:51:09 -0300

    Agregado numero de version

--- Prototipo/Assets/Scripts/SceneTransition.cs
+++ Prototipo/Assets/Scripts/SceneTransition.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class SceneTransition : MonoBehaviour
 {
     public Animator animator;
+    public Text version;
 
     private int levelToLoad;
 
     private void Start()
     {
         Time.timeScale = 1;
+        version.text = "v" + Application.version;
     }
     void Update()
     {
