e45ea6ef4d800eafc8ac5c1a910e4fbb3b9717b6
mbpmm <marianomcr92@gmail.com>
2019-11-09 19:25:41 -0300

    Agregado prueba de parallax

---
+++ Prototipo/Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    public float speed;
+    private float timer;
+    MeshRenderer backRenderer;
+    void Start()
+    {
+        backRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Vector2 offset = new Vector2(0, timer * speed);
+        backRenderer.material.mainTextureOffset = offset;
+    }
+}
