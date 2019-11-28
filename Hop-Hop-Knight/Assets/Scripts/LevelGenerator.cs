f237b384c3f13490558d4e123e477ce73f218c37
mbpmm <marianomcr92@gmail.com>
2019-11-28 12:11:12 -0300

    Actualizado gamescene

--- Hop-Hop-Knight/Assets/Scripts/LevelGenerator.cs
+++ Hop-Hop-Knight/Assets/Scripts/LevelGenerator.cs
@@ -10,89 +10,26 @@ public class LevelGenerator : MonoBehaviour
     public float gemRate;
     private bool hasGem;
     public float timerPULG;
-    public int maxEasy = 7;
-    public int maxMedium = 14;
-    public int maxHard = 19;
+    public int[] difficulties;
+    public int aux;
     // Start is called before the first frame update
     void Start()
     {
-        if ((UnityEngine.Random.Range(0f, 1f)) <= gemRate)
-        {
-            hasGem = true;
-        }
-        else
-        {
-            hasGem = false;
-        }
+        hasGem = (Random.Range(0f, 1f)) <= gemRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (GameManager.Get().score == 0 )
-        //{
-        //    if (transform.position.y < generationPoint.position.y-8f)
-        //    {
-        //        transform.position = new Vector3(transform.position.x, transform.position.y + 8f, transform.position.z);
-        //        randomBlock = 3;
-        //        GameObject go = ObjectPool.instance.GetPooledObject(randomBlock.ToString());
-
-        //        go.transform.position = transform.position;
-        //    }
-            
-        //}
-        //else if ((GameManager.Get().score > 0 && GameManager.Get().score < 15) && !GameManager.Get().player.powerUpActivated)
-        //{
-        //    timerPULG = 0;
-        //    if (transform.position.y < generationPoint.position.y)
-        //    {
-        //        transform.position = new Vector3(transform.position.x, transform.position.y + 8f, transform.position.z);
-        //        randomBlock = Random.Range(1, 6);
-        //        GameObject go = ObjectPool.instance.GetPooledObject(randomBlock.ToString());
-        //        HasGem();
-        //        go.transform.position = transform.position;
-        //    }
-        //}
-        //else if ((GameManager.Get().score >= 15 && GameManager.Get().score < 30) && !GameManager.Get().player.powerUpActivated)
-        //{
-        //    timerPULG = 0;
-        //    if (transform.position.y < generationPoint.position.y)
-        //    {
-        //        transform.position = new Vector3(transform.position.x, transform.position.y + 8f, transform.position.z);
-        //        randomBlock = Random.Range(1, 13);
-        //        GameObject go = ObjectPool.instance.GetPooledObject(randomBlock.ToString());
-        //        HasGem();
-        //        go.transform.position = transform.position;
-        //    }
-        //}
-        //else if (GameManager.Get().score >= 30 && !GameManager.Get().player.powerUpActivated)
-        //{
-        //    timerPULG = 0;
-        //    if (transform.position.y < generationPoint.position.y)
-        //    {
-        //        transform.position = new Vector3(transform.position.x, transform.position.y + 8f, transform.position.z);
-        //        randomBlock = Random.Range(1, 17);
-        //        GameObject go = ObjectPool.instance.GetPooledObject(randomBlock.ToString());
-        //        HasGem();
-        //        go.transform.position = transform.position;
-        //    }
-        //}
-
         if (GameManager.Get().score >= 0 && !GameManager.Get().player.powerUpActivated)
         {
             timerPULG = 0;
             randomBlock = 3;
-            if ((GameManager.Get().score > 0 && GameManager.Get().score < 15))
+            
+            if (GameManager.Get().score > 0)
             {
-                randomBlock = Random.Range(1, maxEasy);
-            }
-            if ((GameManager.Get().score >= 15 && GameManager.Get().score < 30))
-            {
-                randomBlock = Random.Range(1, maxMedium);
-            }
-            if (GameManager.Get().score >= 30)
-            {
-                randomBlock = Random.Range(1, maxHard);
+                aux = GameManager.Get().score / 15;
+                randomBlock = Random.Range(1, difficulties[aux]);
             }
 
             if (transform.position.y < generationPoint.position.y - 8f)
@@ -105,7 +42,6 @@ public class LevelGenerator : MonoBehaviour
                 }
                 go.transform.position = transform.position;
             }
-
         }
 
         if (GameManager.Get().player.powerUpActivated)
@@ -130,21 +66,12 @@ public class LevelGenerator : MonoBehaviour
 
     public void HasGem()
     {
-        if ((Random.Range(0f, 1f)) <= gemRate)
-        {
-            hasGem = true;
-        }
-        else
-        {
-            hasGem = false;
-        }
+        hasGem = (Random.Range(0f, 1f)) <= gemRate;
 
         if (hasGem)
         {
             GameObject go = ObjectPool.instance.GetPooledObject("Gem");
 
             go.transform.position = new Vector3(transform.position.x + Random.Range(-5f, 5f), transform.position.y + 3f, transform.position.z);
-        }
-        
-    }
+        }    }
 }
