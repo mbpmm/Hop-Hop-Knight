a96d7d18028090e818b698fa9b9a107a3c97a5ea
mbpmm <marianomcr92@gmail.com>
2019-12-10 01:41:18 -0300

    Arreglado musica de tutorial repitiendose,

--- Hop-Hop-Knight/Assets/Scripts/Tutorial.cs
+++ Hop-Hop-Knight/Assets/Scripts/Tutorial.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    public GameObject blockDestructionPoint;
+
+    private void Start()
+    {
+        blockDestructionPoint = GameObject.Find("BlockDestructionPoint");
+    }
+    private void Update()
+    {
+        if (transform.position.y < blockDestructionPoint.transform.position.y)
+        {
+            gameObject.SetActive(false);
+        }
+    }
     public void TutorialSound()
     {
         AkSoundEngine.PostEvent("hud_hand", gameObject);
